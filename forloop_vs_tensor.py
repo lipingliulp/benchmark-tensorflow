f7565634b0768a87a93a9027503237260936c9be
Liping Liu <liping.liulp@gmail.com>
2016-12-03 23:10:46 -0500

    clean up code a little

--- forloop_vs_tensor.py
+++ forloop_vs_tensor.py
@@ -1,46 +1,46 @@
+'''
+Computation time comparison: tensor calculation v.s. for-loop with list of tensors 
+Author: Li-Ping Liu 
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
+from __future__ import print_function
+
 import tensorflow as tf
 import numpy as np
 import time
 
+size = 10000
 
-size = 1000
-npa = np.random.rand(size)
-npb = np.random.rand(size)
+# allocate two vectors, use different ways to calculate c = a + b 
+a_np = np.random.rand(size)
+b_np = np.random.rand(size)
 
-a = tf.constant(npa)
-b = tf.constant(npb)
+a_tf = tf.constant(a_np)
+b_tf = tf.constant(b_np)
 
-a_list = tf.unpack(a)
-b_list = tf.unpack(b)
+a_tflist = tf.unpack(a_tf)
+b_tflist = tf.unpack(b_tf)
 
-npa_list = npa.tolist()
-npb_list = npb.tolist()
-
-
-
-# time of python list operation
-t0_start = time.clock()
-npc_list = list()
-for i in xrange(size):
-    npc_list.append(npa_list[i] + npb_list[i])
-npc0 = np.array(npc_list)
-t0_nplist = time.clock() - t0_start
-print("Numpy calculation with lists takes time %f seconds." % t0_nplist)
+a_nplist = a_np.tolist()
+b_nplist = b_np.tolist()
 
 # construct graph with list
 t1_start =  time.clock()
 
-c_list = list()
+c_tflist = list()
 for i in xrange(size):
-    c_list.append(a_list[i] + b_list[i])
-c1 = tf.pack(c_list)
+    c_tflist.append(a_tflist[i] + b_tflist[i])
+c_tf1 = tf.pack(c_tflist)
 
 t1_graph = time.clock() - t1_start
 print("Construction of a graph with lists takes time %f seconds." % t1_graph)
 
 # construct graph with vector 
 t2_start =  time.clock()
-c2 = a + b
+
+c_tf2 = a_tf + b_tf
+
 t2_graph = time.clock() - t2_start
 print("Construction of a graph with tensors takes time %f seconds." % t2_graph)
 
@@ -48,16 +48,30 @@ session = tf.Session()
 
 # calculation with graph constructed from lists 
 t3_start =  time.clock()
-npc1 = session.run(c1)
+
+c_np1 = session.run(c_tf1)
+
 t3_calculation =  time.clock() - t3_start
 print("Execution of a graph with lists takes time %f seconds." % t3_calculation)
 
 # calculation with graph constructed from vectors 
 t4_start =  time.clock()
-npc2 = session.run(c2)
+
+c_np2 = session.run(c_tf2)
+
 t4_calculation =  time.clock() - t4_start
 print("Execution of a graph with tensors takes time %f seconds." % t4_calculation)
 
-assert(np.sum(npc0 != npc1) == 0)
-assert(np.sum(npc1 != npc2) == 0)
+# you may think that python list operations take too much time
+# time of numpy calculation with list 
+t5_start = time.clock()
+c_nplist = list()
+for i in xrange(size):
+    c_nplist.append(a_nplist[i] + b_nplist[i])
+c_np0 = np.array(c_nplist)
+t5_nplist = time.clock() - t5_start
+print("Numpy calculation with lists takes time %f seconds." % t5_nplist)
+
+assert(np.sum(c_np0 != c_np1) == 0)
+assert(np.sum(c_np1 != c_np2) == 0)
 
